9c4981e98ed5efb3f32a28d996a0547109c96760
Yevgeni Starostin <nido-svj@mail.ru>
2017-03-24 00:47:41 +0300

    Implemented some basic features

--- db_shell.py
+++ db_shell.py
@@ -3,20 +3,28 @@ from os import path, environ as env
 from sys import path as syp
 syp.append(path.expanduser('~/lib'))
 from SciDataBase import SciDataBase
+import sys
 data_file = env['SCIDAT']
 db = SciDataBase(data_file)
 
-def print_filtered(tags):
-    for i in db.filter_tags(list(tags)):
-        print(i)
+class State:
+    def __init__(self):
+        self.psl = []
 
-#cur_tags = []
-#cmd = ''
-#while cmd != 'exit':
-#    cmd = input(cur_tags.sort())
-#    try:
-#        exec(cmd)
-#        print('here')
-#    except:
-#        fcmd = 'db.{}'.format(cmd)
-#        exec(fcmd)
+s = State()
+def ud():
+    sys.ps1 = '{} '.format(s.psl)
+    #exec('sys.ps1 = {} '.format(s.psl))
+ud()
+def print_filtered(tags):
+    for i in db.filter_tags(tags):
+        db.print_item(db.extract(i))
+
+def app(data):
+    if type(data) == str:
+        s.psl.append(data)
+    else:
+        for tag in data:
+            s.psl.append(tag)
+    print_filtered(s.psl)
+    ud()
